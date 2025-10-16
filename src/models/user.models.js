c66dbcc3712ab795f2f1a0c8ca7b0f61502d33ef
Harkirat Chadha <harkiratchadha@gmail.com>
2025-10-16 19:26:30 +0530

    Added opaque Token

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -100,4 +100,16 @@ userSchema.methods.generateRefreshToken = function () {
     }
   );
 };
+userSchema.methods.generateTemporaryToken = function () {
+  const unHashedToken = crypto.randomBytes(20).toString("hex");
+
+  const hashedToken = crypto
+    .createHash("sha256")
+    .update(unHashedToken)
+    .digest("hex");
+
+  const tokenExpiry = Date.now() + 20 * 60 * 1000; // 20 mins
+  return { unHashedToken, hashedToken, tokenExpiry };
+};
+
 export const User = mongoose.model("User", userSchema);
