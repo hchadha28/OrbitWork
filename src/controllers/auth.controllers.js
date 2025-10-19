e19a0eea99b5f35331db5b218fdf6644fe29ada2
Harkirat Chadha <harkiratchadha@gmail.com>
2025-10-20 02:29:26 +0530

    Added logout API

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -3,6 +3,7 @@ import { ApiResponse } from "../utils/api-response.js";
 import { asyncHandler } from "../utils/async-handler.js";
 import { ApiError } from "../utils/api-error.js";
 import { emailVerificationMailgenContent, sendEmail } from "../utils/mail.js";
+import { secureHeapUsed } from "crypto";
 
 const generateAccessAndRefreshTokens = async (userId) => {
   try {
@@ -117,4 +118,38 @@ const login = asyncHandler(async (req, res) => {
       )
     );
 });
-export { registerUser, login };
+const logout = asyncHandler(async (req, res) => {
+  /**
+   * first check if the user is actually valid (currently logged in) or not
+   *                              -> done using middleware verifyJWT in routes
+   * if he is we expire the access and refresh tokens
+   */
+
+  // clean the database
+  await User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $set: {
+        refreshToken: "",
+      },
+    },
+    {
+      new: true,
+    }
+  );
+
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(
+      new ApiResponse(200, {}, "User logged out")
+    )
+});
+
+export { registerUser, login, logout };
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,7 +1,15 @@
 import { Router } from "express";
-import { login, registerUser } from "../controllers/auth.controllers.js";
+import {
+  login,
+  logout,
+  registerUser,
+} from "../controllers/auth.controllers.js";
 import { validate } from "../middlewares/validator.middleware.js";
-import { userRegisterValidator, userLoginValidator } from "../validators/index.js";
+import {
+  userRegisterValidator,
+  userLoginValidator,
+} from "../validators/index.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 // validate is the true middleware, we first pass our data through our own custom error checker,
@@ -9,5 +17,8 @@ const router = Router();
 
 router.route("/register").post(userRegisterValidator(), validate, registerUser);
 router.route("/login").post(userLoginValidator(), validate, login);
+// get and post are same in logout
+
+router.route("/logout").post(verifyJWT, logout);
 
 export default router;
