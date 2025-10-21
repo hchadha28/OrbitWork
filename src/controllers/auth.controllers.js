4755f49e83afd51c05308f686288fae17e78cd4a
Harkirat Chadha <harkiratchadha@gmail.com>
2025-10-21 16:10:59 +0530

    Added api endpoints

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -3,7 +3,7 @@ import { ApiResponse } from "../utils/api-response.js";
 import { asyncHandler } from "../utils/async-handler.js";
 import { ApiError } from "../utils/api-error.js";
 import { emailVerificationMailgenContent, sendEmail } from "../utils/mail.js";
-import { secureHeapUsed } from "crypto";
+import jwt from "jsonwebtoken";
 
 const generateAccessAndRefreshTokens = async (userId) => {
   try {
@@ -170,12 +170,115 @@ const verifyEmail = asyncHandler(async (req, res) => {
     emailVerificationExpiry: { $gt: Date.now() },
   });
 
-  if(!user) throw new ApiError(400, "Token is invalid or expired")
-});
-const changePassword = asyncHandler(async (req, res) => {});
-const refreshAccessToken = asyncHandler(async (req, res) => {});
+  if (!user) throw new ApiError(400, "Token is invalid or expired");
 
-const forgotPassword = asyncHandler(async (req, res) => {});
+  user.emailVerificationToken = undefined;
+  user.emailVerificationExpiry = undefined;
+
+  user.isEmailVerified = true;
+  await user.save({ validateBeforeSave: false });
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, { isEmailVerified: true }, "Email is verified"));
+});
+const resendEmailVerification = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user?._id);
+
+  if (!user) throw new ApiError(404, "User does not exist");
+
+  if (user.isEmailVerified)
+    throw new ApiError(409, "Email is already verified");
+
+  const { unHashedToken, hashedToken, tokenExpiry } =
+    user.generateTemporaryToken();
+
+  user.emailVerificationToken = hashedToken;
+  user.emailVerificationExpiry = tokenExpiry;
+
+  await user.save({ validateBeforeSave: false });
+
+  // sending token to user
+  await sendEmail({
+    email: user?.email,
+    subject: "Please verify your email.",
+    mailgenContent: emailVerificationMailgenContent(
+      user.username,
+      `${req.protocol}://${req.get(
+        "host"
+      )}/api/v1/users/verify-email/${unHashedToken}`
+    ),
+  });
+
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken -emailVerificationToken -emailVerificationExpiry"
+  );
+
+  if (!createdUser) {
+    throw new ApiError(500, "Something went wrong while registering a user.");
+  }
+
+  return res
+    .status(201)
+    .json(new ApiResponse(200, {}, "Mail has been sent on your email."));
+});
+const refreshAccessToken = asyncHandler(async (req, res) => {
+  const incomingRefreshToken =
+    req.cookies.refreshToken || req.body.refreshToken;
+
+  if (!incomingRefreshToken) throw new ApiError(401, "Unauthorized access");
+
+  try {
+    const decodedToken = jwt.verify(
+      incomingRefreshToken,
+      process.env.REFRESH_TOKEN_SECRET
+    );
+
+    const user = await User.findById(decodedToken?._id);
+
+    if (!user) throw new ApiError(401, "Invalid refresh token");
+
+    if (incomingRefreshToken !== user.refreshToken)
+      throw new ApiError(401, "Refresh token is expired");
+
+    const options = {
+      httpOnly: true,
+      secure: true,
+    };
+
+    const { accessToken, refreshToken: newRefreshToken } =
+      await generateAccessAndRefreshTokens(user._id);
+    // update the refresh token now (DB)
+    user.refreshToken = newRefreshToken;
+    await user.save();
+
+    return res
+      .status(200)
+      .cookie("accessToken", accessToken, options)
+      .cookie("refreshToken", newRefreshToken, options)
+      .json(
+        new ApiResponse(
+          200,
+          { accessToken, refreshToken: newRefreshToken },
+          "Access token refreshed"
+        )
+      );
+  } catch (error) {
+    throw new ApiError(401, "Invalid refresh token");
+  }
+});
+
+const forgotPasswordRequest = asyncHandler(async (req, res) => {
+
+});
 const resetPassword = asyncHandler(async (req, res) => {});
 
-export { registerUser, login, logout, getCurrentUser };
+export {
+  registerUser,
+  login,
+  logout,
+  getCurrentUser,
+  verifyEmail,
+  resendEmailVerification,
+  refreshAccessToken,
+};
